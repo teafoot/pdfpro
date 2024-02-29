7bf187a842ce97c6ed8dab5c4fbf526754a88252
karakhanyans <karakhanyansa@gmail.com>
2024-02-29 17:13:09 +0400

    Stripe Plan Param

--- resources/js/Components/Plans.vue
+++ resources/js/Components/Plans.vue
@@ -69,7 +69,7 @@ const plans = [
                     <span class="text-4xl font-extrabold">${{ plan.price }}</span>
                     <span v-if="plan.price !== '0'" class="text-base font-medium">/{{ plan.interval }}</span>
                 </p>
-                <a :href="$page.props.auth.user ? route('stripe.subscription.checkout', {productId: plan.productId, variantId: plan.variantId}) : route('register')"
+                <a :href="$page.props.auth.user ? route('stripe.subscription.checkout', {price: plan.price}) : route('register')"
                    class="mb-6 btn btn-secondary btn-wide text-center mx-auto flex">
                     Choose Plan
                 </a>
