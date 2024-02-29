6bf3b56debe9dcd434118a82e802d6ece7501971
karakhanyans <karakhanyansa@gmail.com>
2024-02-29 17:14:37 +0400

    Remove favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,14 +7,6 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
         <link rel="canonical" href="{{ request()->url() }}">
 
-        <!--Favicon-->
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
-        <link rel="manifest" href="/favicon/site.webmanifest">
-        <meta name="msapplication-TileColor" content="#da532c">
-        <meta name="theme-color" content="#ffffff">
-
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
